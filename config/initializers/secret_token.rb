9d21508fd1376f0fe65a90f63c6afa7e96042267
Matt Van Horn <mattvh@scoopst.com>
2012-05-10 14:42:00 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bjjlife::Application.config.secret_token = 'bf5d0ab536e60be7ccf45bceb1fbfbf724047f88c7309f2c28a4e77c5a599d3aba8abd0ebcf09fb0ceb091df712cd1dd54cc05e01e3a2fa4357a2f2a0c632393'
