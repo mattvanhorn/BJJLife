0b2208020dfb5f0c65bb94ba0ea5cbdb9c605fd2
Matt Van Horn <mattvh@scoopst.com>
2012-05-24 15:55:40 -0700

    Add to the academies list, and change some wording

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,3 +44,169 @@ Academy.find_or_create_by_name("Ralph Gracie Berkeley") do |academy|
   academy.phone_number = "(510) 486-8000"
   academy.website      = "http://www.ralphgracieberkeley.com"
 end
+
+Academy.find_or_create_by_name("NYMAG Naples MMA, BJJ, Muay Thai Kickbxing") do |academy|
+  academy.street       = "Jabz Gym Naples 2151 Trade Center Way"
+  academy.city         = "Naples"
+  academy.us_state     = "FL"
+  academy.postal_code  = "34109"
+  academy.phone_number = "(239) 598 4466"
+  academy.website      = "http://www.NYMAGnaples.com"
+  academy.email        = "NYMAGnaples@gmail.com"
+  academy.instructor   = "Gene Simco (Black Belt 2nd degree)"
+end
+
+Academy.find_or_create_by_name("South Elgin Budokan Martial Arts & Fitness") do |academy|
+  academy.street       = "959 N La Fox St"
+  academy.city         = "South Elgin"
+  academy.us_state     = "IL"
+  academy.postal_code  = "60177"
+  academy.phone_number = "847-888-8866"
+  academy.website      = "http://www.southelginbudokan.com"
+  academy.email        = "southelginbudokan@att.net"
+  academy.instructor   = "T. O'Shaughnessy (rank: 8th Dan Judo)"
+end
+
+Academy.find_or_create_by_name("Carlson Gracie Team Northern Virginia Academy") do |academy|
+  academy.street       = "8253 Backlick Rd. #D"
+  academy.city         = "Lorton"
+  academy.us_state     = "VA"
+  academy.postal_code  = "22079"
+  academy.phone_number = "703-986-9374"
+  academy.website      = "http://www.bjjVA.com"
+  academy.email        = "jiujitsuworld@gmail.com"
+  academy.instructor   = "Ivan Rios (rank: Black Belt)"
+end
+
+Academy.find_or_create_by_name("Lockjaw Jiu-Jitsu Academy") do |academy|
+  academy.street       = "240 West Main Street"
+  academy.city         = "Somerton"
+  academy.us_state     = "AZ"
+  academy.postal_code  = "85350"
+  academy.phone_number = "928-502-0195"
+  academy.website      = "http://www.internationaljujitsu.com"
+  academy.email        = "ija@internationaljujitsu.com"
+  academy.instructor   = "Ralph Villa (rank: 7th Degree Black Belt)"
+end
+
+Academy.find_or_create_by_name("Lockjaw Brazilian Jiu-Jitsu") do |academy|
+  academy.street       = "240 West Main Street"
+  academy.city         = "Somerton"
+  academy.us_state     = "AZ"
+  academy.postal_code  = "85350"
+  academy.phone_number = "928-502-0195"
+  academy.website      = "http://www.internationaljujitsu.com"
+  academy.email        = "ija@internationaljujitsu.com"
+  academy.instructor   = "Ralph Villa (rank: BJJ Brown Belt)"
+end
+
+Academy.find_or_create_by_name("Rockville Martial Arts") do |academy|
+  academy.street       = "1331-O Rockville Pike"
+  academy.city         = "Rockville"
+  academy.us_state     = "MD"
+  academy.postal_code  = "20852"
+  academy.phone_number = "301-251-4000"
+  academy.website      = "http://www.facebook.com/RockvilleBrazilianJiuJitsu"
+  academy.email        = "zimmed03@gmail.com"
+  academy.instructor   = "David Zimmerman (rank: 4 Stripe Blue)"
+end
+
+Academy.find_or_create_by_name("Reaction MMA and Brazilian Jiu-Jitsu") do |academy|
+  academy.street       = "125 Eisenhour Court"
+  academy.city         = "Nicholasville"
+  academy.us_state     = "KY"
+  academy.postal_code  = "40503"
+  academy.phone_number = "8599132013"
+  academy.website      = "http://www.teamreactionmma.com"
+  academy.email        = "robsteel@live.com"
+  academy.instructor   = "Rob Nickerson (rank: Brown)"
+end
+
+Academy.find_or_create_by_name("Huron Valley Jiu Jitsu") do |academy|
+  academy.street       = "302 highland ave"
+  academy.city         = "milford"
+  academy.us_state     = "MI"
+  academy.postal_code  = "48381"
+  academy.phone_number = "248-640-5239"
+  academy.website      = "http://www.hvjj.webstarts.com"
+  academy.email        = "hvjj@comcast.net"
+  academy.instructor   = "Greg Breen (rank: Four stripe blue)"
+end
+
+Academy.find_or_create_by_name("Manassas Mixed Martial Arts") do |academy|
+  academy.street       = "10521 Croatia Way"
+  academy.city         = "Manassas"
+  academy.us_state     = "VA"
+  academy.postal_code  = "20109"
+  academy.phone_number = "703-594-6220"
+  academy.website      = "http://www.ManassasMMA.com"
+  academy.email        = "manassasmma@gmail.com"
+  academy.instructor   = "Dave Trader (rank: Black Belt - 2nd degree)"
+end
+
+Academy.find_or_create_by_name("Gracie Jiu-Jitsu Training Center") do |academy|
+  academy.street       = "9854 South 700 East"
+  academy.city         = "Sandy"
+  academy.us_state     = "UT"
+  academy.postal_code  = "84070"
+  academy.phone_number = "801-347-3471"
+  academy.website      = "http://www.graciejiu-jitsu.us"
+  academy.email        = "graciejiujitsu@instructor.net"
+  academy.instructor   = "Michael A. Hermosillo Sr. (rank: 3rd Degree Black Belt)"
+end
+
+Academy.find_or_create_by_name("Renzo Gracie Academy New York City") do |academy|
+  academy.street       = "224 West 30th Street"
+  academy.city         = "New York"
+  academy.us_state     = "NY"
+  academy.postal_code  = "10001"
+  academy.phone_number = "212.279.6724"
+  academy.website      = "www.renzogracie.com"
+  academy.email        = "info@renzogracie.com"
+  academy.instructor   = "Renzo Gracie"
+end
+
+Academy.find_or_create_by_name("Renzo Gracie Academy Holmdel") do |academy|
+  academy.street       = "Kohl's Plaza 2145 Highway 35"
+  academy.city         = "Holmdel"
+  academy.us_state     = "NJ"
+  academy.postal_code  = "07733"
+  academy.phone_number = "732.888.9399"
+  academy.website      = "http://www.renzogracienj.com"
+  academy.email        = "info@renzogracienj.com"
+  academy.instructor   = "Rolles Gracie"
+end
+
+Academy.find_or_create_by_name("Renzo Gracie Academy Stamford") do |academy|
+  academy.street       = "103 Atlantic St."
+  academy.city         = "Stamford"
+  academy.us_state     = "CT"
+  academy.postal_code  = "06901"
+  academy.website      = "http://www.graciect.com"
+  academy.email        = "info@graciect.com"
+  academy.instructor   = "Igor and Gregor Gracie"
+end
+
+Academy.find_or_create_by_name("Marcelo Garcia's New York City BJJ Academy") do |academy|
+  academy.street       = "25 W 36th St."
+  academy.unit         = "6th Floor"
+  academy.city         = "New York"
+  academy.us_state     = "NY"
+  academy.postal_code  = "10018"
+  academy.phone_number = "212.947.0300"
+  academy.website      = "http://www.marcelogarciajj.com/"
+  academy.email        = "nyc@marcelogarciajj.com"
+  academy.instructor   = "Marcelo Garcia"
+end
+
+# Academy.find_or_create_by_name("") do |academy|
+#   academy.street       = ""
+#   academy.unit         = ""
+#   academy.city         = ""
+#   academy.us_state     = ""
+#   academy.postal_code  = ""
+#   academy.website      = ""
+#   academy.email        = ""
+#   academy.instructor   = ""
+# end
+
