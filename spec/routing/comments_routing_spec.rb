f960eef21015eac817d7dc4e262aa12323e271b5
Matt Van Horn <mattvh@scoopst.com>
2012-06-05 11:27:57 -0700

    hmm. lost my spec fix, due to weak git-fu.

--- spec/routing/comments_routing_spec.rb
+++ spec/routing/comments_routing_spec.rb
@@ -1,14 +1,6 @@
 require 'spec_helper'
 
 describe "comments routing" do
-  it "routes the new comment page for a post" do
-    { :get => "posts/42/comments/new" }.
-      should route_to(
-        :controller => "comments",
-        :action => "new",
-        :post_id => '42'
-      )
-  end
 
   it "routes to the create method" do
     { :post => "/posts/42/comments" }.
