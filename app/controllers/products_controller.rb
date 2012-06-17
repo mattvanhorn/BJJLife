e1409909556966e8bf7ec530a2e8fa22fb98410e
Matt Van Horn <mattvh@scoopst.com>
2012-06-17 10:41:30 -0700

    controller specs for products page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,10 @@
 class ProductsController < ApplicationController
+  expose :products
   expose :product
+
+  def index
+    exhibit_exposed :products
+  end
 
   def show
     exhibit_exposed :product
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,13 +1,33 @@
 require 'spec_helper'
 
 describe ProductsController do
-  let(:resource){ mock_model(Product, :save => true) }
-
+  let(:products){ [] }
+  let(:product){ Object.new }
+    
   before(:each) do
-    Product.stub(:new => resource)
+    Product.stub(:scoped).and_return(products)
+    products.stub(:find).and_return(product)
+  end
+
+  describe "#index" do
+    it "exposes the products" do
+      Product.should_receive(:scoped).and_return(products)
+      controller.products.should equal(products)
+    end
+    
+    it "exhibits all the products" do
+      controller.should_receive(:exhibit_exposed).with(:products)
+      get :index
+    end
   end
 
   describe "#show" do
+    it "exposes a product" do
+      products.should_receive(:find).with('42').and_return(product)
+      controller.stub(:params => {:id => '42'})
+      controller.product.should equal(product)
+    end
+    
     it "exhibits a product" do
       controller.should_receive(:exhibit_exposed).with(:product)
       get :show, :id => '42'
