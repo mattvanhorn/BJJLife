d1f3dfac25a085cae8293094cb4a93bfaa07990d
Matt Van Horn <mattvh@scoopst.com>
2012-06-18 18:33:43 -0700

    make carrierwave use local storage in test

--- app/uploaders/product_photo_uploader.rb
+++ app/uploaders/product_photo_uploader.rb
@@ -7,26 +7,25 @@ class ProductPhotoUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
-  # include Sprockets::Helpers::RailsHelper
-  # include Sprockets::Helpers::IsolatedHelper
+  include Sprockets::Helpers::RailsHelper
+  include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
-  storage :fog
+  # storage :fog
   # storage :file
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "carrierwave/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+    # For Rails 3.1+ asset pipeline compatibility:
+    asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+    # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,23 @@
 CarrierWave.configure do |carrierwave|
-  carrierwave.root = Rails.root.join('tmp')
-  carrierwave.cache_dir = 'carrierwave'
+  carrierwave.cache_dir = 'tmp'
 
-  carrierwave.fog_credentials = {
-     :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-     :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-     :provider => 'AWS',
-     :region => ENV['S3_REGION']
-   }
-   carrierwave.fog_directory = ENV['S3_BUCKET']
-   carrierwave.fog_public = true
-   carrierwave.fog_host = "https://s3-#{ENV['S3_REGION']}.amazonaws.com/#{ENV['S3_BUCKET']}" if Rails.env.development?
+  if Rails.env.test? or Rails.env.cucumber?
+    carrierwave.storage = :file
+    carrierwave.enable_processing = false
+
+  else
+    carrierwave.storage = :fog
+    carrierwave.fog_credentials = {
+       :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+       :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+       :provider => 'AWS',
+       :region => ENV['S3_REGION']
+     }
+     carrierwave.fog_directory = ENV['S3_BUCKET']
+     carrierwave.fog_public = true
+     carrierwave.fog_host = "https://s3-#{ENV['S3_REGION']}.amazonaws.com/#{ENV['S3_BUCKET']}" if Rails.env.development?
+   end
 end
+
+
+
