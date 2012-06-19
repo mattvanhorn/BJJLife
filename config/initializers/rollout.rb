e2bb3dd47d2dd45b0773001342df0ead8f538540
Matt Van Horn <mattvh@scoopst.com>
2012-06-18 19:14:29 -0700

    skip before filters on rollout UI

--- config/initializers/rollout.rb
+++ config/initializers/rollout.rb
@@ -4,3 +4,9 @@ RolloutUi.wrap($rollout)
 RolloutUi::Server.use Rack::Auth::Basic do |username, password|
   username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
 end
+
+Rails.application.config.to_prepare do
+  RolloutUi::ApplicationController.class_eval do
+    skip_before_filter :init_blog, :tag_guest, :store_location
+  end
+end
