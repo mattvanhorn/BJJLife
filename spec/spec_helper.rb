92acca04423e33458cc2bf5a890beec7a9231c9f
Matt Van Horn <mattvanhorn@gmail.com>
2013-04-04 09:48:14 -0700

    silence tddium warning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,7 +18,7 @@ require 'shoulda-matchers'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir[Rails.root.join("spec/support/**/*.rb")].sort.each {|f| require f}
 
 RSpec.configure do |specs|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
