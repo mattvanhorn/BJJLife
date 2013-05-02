1ec5e491e09554dd1b54f0c18d3d8c1653b60966
Matt Van Horn <mattvanhorn@gmail.com>
2013-05-02 15:02:46 -0700

    deodorize omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,12 +1,26 @@
 class OmniAuth::Strategies::Identity
   def callback_phase
     unless identity
-      return fail!('omniauth.email_and_password_missing') if (request['auth_key'].blank? && request['password'].blank?)
-      return fail!('omniauth.email_missing') if request['auth_key'].blank?
-      return fail!('omniauth.password_missing') if request['password'].blank?
+      return fail!('omniauth.email_and_password_missing') if missing_credentials?
+      return fail!('omniauth.email_missing') if missing_email?
+      return fail!('omniauth.password_missing') if missing_password?
       return fail!('omniauth.invalid_credentials')
     end
     super
+  end
+
+  private
+
+  def missing_credentials?
+    missing_email? && missing_password?
+  end
+
+  def missing_email?
+    request['auth_key'].blank?
+  end
+
+  def missing_password?
+    request['password'].blank?
   end
 end
 
