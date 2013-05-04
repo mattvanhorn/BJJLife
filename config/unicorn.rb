93042f3943e6bacae9edab894998246462b30856
Matt Van Horn <mattvanhorn@gmail.com>
2013-05-04 09:43:46 -0700

    troubleshooting post-deploy issue

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,3 +3,24 @@ rails_env = ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'production'
 worker_processes (rails_env == 'production' ? 4 : 1) # amount of unicorn workers to spin up
 timeout 30
 preload_app true
+
+ before_fork do |server, worker|
+
+   Signal.trap 'TERM' do
+     puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+     Process.kill 'QUIT', Process.pid
+   end
+
+   defined?(ActiveRecord::Base) and
+     ActiveRecord::Base.connection.disconnect!
+ end
+
+ after_fork do |server, worker|
+
+   Signal.trap 'TERM' do
+     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+   end
+
+   defined?(ActiveRecord::Base) and
+     ActiveRecord::Base.establish_connection
+ en
