9ba8dde76012ee78cc3cfee7815ff6c498c2bdc8
Matt Van Horn <mattvanhorn@gmail.com>
2013-05-06 21:37:05 -0700

    Fix for CI server (tddium)

--- spec/support/active_record.rb
+++ spec/support/active_record.rb
@@ -2,11 +2,8 @@ require 'active_record'
 require 'yaml'
 require 'support/rails_minimum'
 
-dbconfig = YAML::load(File.open("#{RAILS_ROOT}/config/database.yml"))
+dbconfig = YAML::load(ERB.new(File.read("#{RAILS_ROOT}/config/database.yml")).result)
 ActiveRecord::Base.configurations = dbconfig
 
 require 'nulldb_rspec'
 NullDB.configure {|ndb| def ndb.project_root;RAILS_ROOT;end}
-
-
-
