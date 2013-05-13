8110bd68beb81ea14ff084d40830369edf11a8e3
Matt Van Horn <mattvanhorn@gmail.com>
2013-05-13 16:07:43 -0700

    deodorize journal page objects

--- features/support/page_objects/journal_pages.rb
+++ features/support/page_objects/journal_pages.rb
@@ -1,22 +1,18 @@
 class NewJournalPage < SitePrism::Page
-  URL = Rails.application.routes.url_helpers.new_journal_path
-  set_url URL
-  set_url_matcher %r(#{URL})
+  include ::CommonPageBehavior
+  init_url_and_matcher(Rails.application.routes.url_helpers.new_journal_path)
 end
 
 class JournalPage < SitePrism::Page
-  URL = Rails.application.routes.url_helpers.journal_path
-  set_url URL
-  set_url_matcher %r(#{URL})
+  include ::CommonPageBehavior
+  init_url_and_matcher(Rails.application.routes.url_helpers.journal_path)
 end
 
 class NewJournalEntryPage < SitePrism::Page
-  URL = Rails.application.routes.url_helpers.new_journal_entry_path
-  set_url URL
-  set_url_matcher %r(#{URL})
+  include ::CommonPageBehavior
+  init_url_and_matcher(Rails.application.routes.url_helpers.new_journal_entry_path)
 
   element :title_field, "input[name='journal_entry[title]']"
   element :body_field,  "textarea[name='journal_entry[body]']"
   element :post_entry_btn, "input[name='commit']"
-
 end
