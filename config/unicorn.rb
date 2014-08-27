2a93e203d299e21989a2d64648e9042d3e4a56be
Matt Van Horn <mattvanhorn@gmail.com>
2014-08-27 14:52:55 -0700

    unicorn change to single worker

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 rails_env = ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'production'
 
-worker_processes (rails_env == 'production' ? 4 : 1) # amount of unicorn workers to spin up
+worker_processes (rails_env == 'production' ? 1 : 1) # amount of unicorn workers to spin up
 timeout 30
 preload_app true
 
